38df72ab048fa2d15e541d9caf2e2350d21fb525
hapo-nghialuu <nghialt@haposoft.com>
2022-06-13 16:50:17 +0700

    Revert file webpack.mix.js with npm build

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,5 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('public/app/js/app.js', 'public/app/public/js')
+mix.js('resources/js/app.js', 'public/js')
     .vue()
-    .sass('public/app/scss/app.scss', 'public/app/public/css');
+    .sass('resources/sass/app.scss', 'public/css');
