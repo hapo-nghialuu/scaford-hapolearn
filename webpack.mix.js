b04a495b7fcfcbdc41b2e7c47dcbcd0c1e1b6585
hapo-nghialuu <nghialt@haposoft.com>
2022-06-14 16:49:32 +0700

    Add example code html header

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,6 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .copy('resources/assets/images', 'public/images')
+    .copy('resources/assets/fonts', 'public/fonts');
